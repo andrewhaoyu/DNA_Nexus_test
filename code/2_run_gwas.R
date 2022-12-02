89555ba4e6ffb2f1a110a53034d8913f9f663e67
Zhang <andrew.haoyu@gmail.com>
2022-12-02 12:44:40 -0500

    update

--- code/2_run_gwas.R
+++ code/2_run_gwas.R
@@ -1,3 +1,13 @@
 #Goal:run GWAS with plink2 with the available data in each consortium
-
-plink2_command = paste0("")
+soft_dir = paste0("../Software/")
+data_dir = paste0("../Data/")
+system("mkdir temp_result")
+out_dir = "temp_result/"
+#run GWAS with plink2
+plink2_command = paste0(soft_dir, "plink2 ",
+                        "--bfile ",data_dir,"AMR ",
+                        "--pheno ",data_dir,"AMR.phen ",
+                        "--covar ",data_dir,"AMR.covar ",
+                        "--linear ",
+                        "--out ",out_dir,"AMR")
+system(plink2_command)
