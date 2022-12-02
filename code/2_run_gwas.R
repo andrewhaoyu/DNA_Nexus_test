b605674d1f59c94fe5f117212cda8daaffb170f7
Zhang <andrew.haoyu@gmail.com>
2022-12-02 12:48:52 -0500

    udpate

--- code/2_run_gwas.R
+++ code/2_run_gwas.R
@@ -3,11 +3,13 @@ soft_dir = paste0("../Software/")
 data_dir = paste0("../Data/")
 system("mkdir temp_result")
 out_dir = "temp_result/"
+eth_vec = c("AFR", "AMR", "EAS", "EUR", "SAS")
+i = 2
 #run GWAS with plink2
 plink2_command = paste0(soft_dir, "plink2 ",
-                        "--bfile ",data_dir,"AMR ",
-                        "--pheno ",data_dir,"AMR.phen ",
-                        "--covar ",data_dir,"AMR.covar ",
+                        "--bfile ",data_dir,eth_vec[i]," ",
+                        "--pheno ",data_dir,eth_vec[i],".phen ",
+                        "--covar ",data_dir,eth_vec[i],".covar ",
                         "--linear ",
                         "--out ",out_dir,"AMR")
 system(plink2_command)
